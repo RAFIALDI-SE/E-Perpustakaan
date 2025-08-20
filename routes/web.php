f3f4c4363bd36b115a497249967167492df1fab9
RAFIALDI-SE <rafialdiputra1402@gmail.com>
2025-08-20 11:48:13 +0700

    fix endpoint untuk kebutuhan logika

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,17 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\BukuController;
+use App\Http\Controllers\PeminjamanController;
+use App\Models\Peminjaman;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-
-Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
-Route::post('/register', [AuthController::class, 'register']);
-Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 Route::get('/kategori', [KategoriController::class, 'index'])->name('kategori.index');
 Route::get('/kategori/create', [KategoriController::class, 'create'])->name('kategori.create');
@@ -23,4 +20,35 @@ Route::get('/kategori/edit/{id}', [KategoriController::class, 'edit'])->name('ka
 Route::put('/kategori/update/{id}', [KategoriController::class, 'update'])->name('kategori.update');
 Route::delete('/kategori//delete/{id}', [KategoriController::class, 'destroy'])->name('kategori.destroy');
 
+Route::get('/buku', [BukuController::class, 'index'])->name('buku.index');
+Route::get('/buku/create', [BukuController::class, 'create'])->name('buku.create');
+Route::post('/buku/store', [BukuController::class, 'store'])->name('buku.store');
+Route::get('/buku/edit/{id}', [BukuController::class, 'edit'])->name('buku.edit');
+Route::put('/buku/update/{id}', [BukuController::class, 'update'])->name('buku.update');
+Route::delete('/buku//delete/{id}', [BukuController::class, 'destroy'])->name('buku.destroy');
 
+Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
+Route::post('/register', [AuthController::class, 'register']);
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::get('/dashboard', [AuthController::class, "index"]);
+
+Route::get('/peminjaman', [PeminjamanController::class, 'index'])->name('peminjaman.index');
+Route::get('/peminjaman/create', [PeminjamanController::class, 'create'])->name('peminjaman.create');
+Route::post('/peminjaman/store', [PeminjamanController::class, 'store'])->name('peminjaman.store');
+Route::put('/peminjaman/{id}/kembalikan', [PeminjamanController::class, 'kembalikan'])->name('peminjaman.kembalikan');
+
+
+
+// Untuk validasi peminjaman (admin)
+Route::get('/admin/peminjaman', [PeminjamanController::class, 'adminIndex'])->name('admin.index');
+Route::post('/admin/peminjaman/{id}/validasi', [PeminjamanController::class, 'validasi'])->name('admin.peminjaman.validasi');
+
+// Untuk pengembalian oleh admin
+Route::get('/admin/pengembalian', [PeminjamanController::class, 'adminPengembalian'])->name('admin.kembali');
+Route::post('/admin/pengembalian/{id}/kembalikan', [PeminjamanController::class, 'adminKembalikan'])->name('admin.peminjaman.kembalikan');
+
+
+Route::get('/user', [PeminjamanController::class, 'user']);
