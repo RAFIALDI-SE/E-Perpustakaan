fea26f0f3a1ffb8072093a8e6256bff7cbe1c7c4
RAFIALDI-SE <rafialdiputra1402@gmail.com>
2025-08-20 11:45:52 +0700

    Membuat Endpoint Kategori untuk CRUD kategori sisi Admin

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\KategoriController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -14,3 +15,12 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::get('/kategori', [KategoriController::class, 'index'])->name('kategori.index');
+Route::get('/kategori/create', [KategoriController::class, 'create'])->name('kategori.create');
+Route::post('/kategori/store', [KategoriController::class, 'store'])->name('kategori.store');
+Route::get('/kategori/edit/{id}', [KategoriController::class, 'edit'])->name('kategori.edit');
+Route::put('/kategori/update/{id}', [KategoriController::class, 'update'])->name('kategori.update');
+Route::delete('/kategori//delete/{id}', [KategoriController::class, 'destroy'])->name('kategori.destroy');
+
+
